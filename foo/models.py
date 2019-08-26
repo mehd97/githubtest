35a04803746096ce78be7c54eda76b51e1910840
mehdi kamani <itsmehd97@gmail.com>
2019-08-26 13:30:07 +0430

    tool model added

--- foo/models.py
+++ foo/models.py
@@ -7,3 +7,11 @@ class TestModel(models.Model):
 
 class UserModel(model.Model):
     name = models.CharField(max_length=20)
+
+
+class ToolModel(model.Model):
+    name = models.CharField(max_length=20)
+    age = models.CharField(max_length=20)
+
+class test:
+    pass
