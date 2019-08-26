6e7e8fa0e2cd6fbbe097a513baf1df5f7f49cf20
itsmehdi97 <itsmehdi97@gmial.com>
2019-08-26 12:55:31 +0430

    Revert "added tool model"
    This reverts commit a2564b49879cb46658495f3169832e6937286633.

--- foo/models.py
+++ foo/models.py
@@ -7,7 +7,3 @@ class TestModel(models.Model):
 
 class UserModel(model.Model):
     name = models.CharField(max_length=20)
-
-
-class TollModel(model.Model):
-    name = models.CharField(max_length=20)
